0217ce3a55bcacffa37cd24210aa6fea6f3b0585
fstltna <fstltna@yahoo.com>
2025-02-01 03:03:48 -0800

    Added new game listing options

--- bin/empwrapper.pl
+++ bin/empwrapper.pl
@@ -8,19 +8,24 @@ use Term::ANSIScreen qw(cls);
 use File::Fetch;
 
 my $EmpireCommand = "/home/empire/empiremanager/bin/empire -r -s empiredirectory.net:6665";
+my $EmpireFeverCommand = "/home/empire/empiremanager/bin/empire -r -s blitz.wolfpackempire.com:4567";
 my $BannerText = "logo.txt";
 my $LESSCMD = "/usr/bin/less";
 my $WhatIsFile = "whatisempire.txt";
 my $EmpireLinks = "empirelinks.txt";
+my $OtherGamesFile = "OtherGames.csv";
 
 ###################################################
 # No changes below here
 ###################################################
 
 
-my $EMP_ver = "1.1.0";
+my $EMP_ver = "1.2.0";
 my $url = 'http://empiredirectory.net/empusers.txt';
+my $GameSubsLink = "https://empiredirectory.net/index.php/game-hosting";
+
 $ENV{'LESSSECURE'} = '1';
+my $ContactUs = "newempiregame\@empiredirectory.net";
 
 my $clear_screen = cls();
 
@@ -52,7 +57,10 @@ sub MainMenu
                             list => [ '1', 'Play Empire',
                                       '2', 'List Sanctuaries',
                                       '3', 'What is Empire',
-                                      '4', 'Empire Links',
+                                      '4', 'Play Fever',
+                                      '5', 'Fever Sanct.',
+                                      '6', 'Empire Links',
+                                      '7', 'Games List',
                                       'q', 'Quit Empire' ] );
 }
 
@@ -60,6 +68,28 @@ sub DisplayLogo
 {
 	# Display the logo file
 	system("cat $BannerText");
+}
+
+sub ShowOtherGames
+{
+	print "Available Games:\n";
+	#print "Game Name\t\t\tGame Connection\t\tWebsite\n";
+	open(my $fh, '<:encoding(UTF-8)', $OtherGamesFile)
+		or die "Could not open file '$OtherGamesFile' $!";
+	while (my $row = <$fh>)
+	{
+		chomp $row;
+		print "==========\n";
+		(my $GameName, my $GameConnect, my $GameWebsite) = split('\t', $row);
+		print "Game Name:\t\t$GameName\n";
+		print "Game Connection:\t$GameConnect\n";
+		print "Game Website:\t\t$GameWebsite\n";
+	}
+	close($fh);
+	print "----------\nTo get your server listed here contact us at $ContactUs\n";
+	print "Get a Empire server of your own for just \$7: $GameSubsLink\n";
+	print "--[ Press Enter To Continue ]--";
+	my $usrword = <STDIN>;
 }
 
 print $clear_screen;
@@ -100,7 +130,34 @@ while (-1)
 	elsif ($menuselection eq "4")
 	{
 		print $clear_screen;
+		system("$EmpireFeverCommand");
+		print "--[ Press Enter To Continue ]--";
+		my $usrword = <STDIN>;
+	}
+	elsif ($menuselection eq "5")
+	{
+		print $clear_screen;
+		DisplayLogo();
+		open(my $fh, '<:encoding(UTF-8)', $file)
+			or die "Could not open file '$file' $!";
+		while (my $row = <$fh>)
+		{
+			chomp $row;
+			print "$row\n";
+		}
+		close($fh);
+		print "--[ Press Enter To Continue ]--";
+		my $usrword = <STDIN>;
+	}
+	elsif ($menuselection eq "6")
+	{
+		print $clear_screen;
 		system("$LESSCMD $EmpireLinks");
+	}
+	elsif ($menuselection eq "7")
+	{
+		print $clear_screen;
+		ShowOtherGames();
 	}
 }
 
